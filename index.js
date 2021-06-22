49044e044b2fc165a764b0246d76d645a1c68d27
AZIZXlaouiti <azizrocke@gmail.com>
2021-06-22 18:20:22 -0400

    commit tree 1133f38b6542060a25c1ac24708db50c490feb16

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+
+function createEmployeeRecord(array){
+    return {
+        firstName : array[0],
+        familyName : array[1],
+        title : array[2],
+        payPerHour : array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array){
+    return records=[
+        createEmployeeRecord(array[0]),
+        createEmployeeRecord(array[1])
+    ]
+}
+// let test1 = createEmployeeRecords([
+//     ["moe", "sizlak", "barkeep", 2],
+//     ["bartholomew", "simpson", "scamp", 3]
+//   ])
